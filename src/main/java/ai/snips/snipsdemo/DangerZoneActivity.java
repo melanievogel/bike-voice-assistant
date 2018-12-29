038c1c8d26e3df0e2ad125cc9b5241baa741fb29
melanie <vogel.melanie@outlook.com>
2018-12-29 10:15:27 +0100

    Added ListView of Danger Zones with Dummy Data.

--- src/main/java/ai/snips/snipsdemo/DangerZoneActivity.java
+++ src/main/java/ai/snips/snipsdemo/DangerZoneActivity.java
@@ -1,8 +1,12 @@
 package ai.snips.snipsdemo;
 
-import android.icu.text.MessagePattern;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DangerZoneActivity extends AppCompatActivity {
 
@@ -11,5 +15,36 @@ public class DangerZoneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.danger_zones);
 
+        final ListView listView = findViewById(R.id.listview);
+
+        String[] values = new String[] { "Steil abfallendes Gelände", "Brücke", "Steil abfallender Hang"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+                listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                //list.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+            }
+        });
     }
 }
---
+++ src/main/java/ai/snips/snipsdemo/DangerZoneObject.java
@@ -0,0 +1,18 @@
+package ai.snips.snipsdemo;
+
+public class DangerZoneObject {
+
+    int id;
+    String name;
+    String distance;
+
+    public DangerZoneObject(int id, String name, String distance){
+
+        this.id = id;
+        this.name = name;
+        this.distance = distance;
+
+    }
+
+
+}
