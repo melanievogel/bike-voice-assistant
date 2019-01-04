e85298c9f26e5b9f95e2dc10f7180420d86d02b9
melanie <vogel.melanie@outlook.com>
2019-01-04 21:55:22 +0100

    Added GPS Request.

--- src/main/java/ai/snips/snipsdemo/DangerZoneActivity.java
+++ src/main/java/ai/snips/snipsdemo/DangerZoneActivity.java
@@ -1,6 +1,16 @@
 package ai.snips.snipsdemo;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -8,6 +18,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
@@ -19,8 +30,11 @@ public class DangerZoneActivity extends AppCompatActivity {
 
     Button addButton;
     EditText inputText;
+    TextView gpsText;
     ArrayList<String> list;
     ArrayAdapter<String> adapter;
+    private LocationManager locationManager;
+    private LocationListener listener;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -29,16 +43,12 @@ public class DangerZoneActivity extends AppCompatActivity {
 
         addButton = findViewById(R.id.button);
         inputText = findViewById(R.id.newItem);
+        gpsText = findViewById(R.id.gpsdata);
 
         list = new ArrayList<String>();
         list.add("Gefährliches Gelände in 1km");
 
         ListView listView = findViewById(R.id.listview);
-
-
-       // for (int i = 0; i < values.length; ++i) {
-         //   list.add(values[i]);
-       // }
 
         adapter = new ArrayAdapter(this,
                 android.R.layout.simple_list_item_1, list);
@@ -46,6 +56,60 @@ public class DangerZoneActivity extends AppCompatActivity {
         clickItem(listView, adapter);
         addItem();
 
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                gpsText.append("\n " + location.getLongitude() + " " + location.getLatitude());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        configure_button();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void configure_button(){
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+
+                //noinspection MissingPermission
+                locationManager.requestLocationUpdates("gps", 5000, 0, listener);
     }
 
     public void addItem(){
