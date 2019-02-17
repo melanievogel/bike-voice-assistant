e70abc8a84ee544b257ce3401a6d5ccafd468f84
melanie <vogel.melanie@outlook.com>
2019-02-17 20:13:38 +0100

    Offline map still not working

--- src/main/java/ai/snips/snipsdemo/MapViewActivity.java
+++ src/main/java/ai/snips/snipsdemo/MapViewActivity.java
@@ -12,21 +12,13 @@ import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.modules.ArchiveFileFactory;
-import org.osmdroid.tileprovider.modules.IArchiveFile;
-import org.osmdroid.tileprovider.modules.OfflineTileProvider;
-import org.osmdroid.tileprovider.tilesource.FileBasedTileSource;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.tileprovider.util.SimpleRegisterReceiver;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Set;
 
-import static android.app.PendingIntent.getActivity;
 import static org.osmdroid.views.overlay.gridlines.LatLonGridlineOverlay.backgroundColor;
 import static org.osmdroid.views.overlay.gridlines.LatLonGridlineOverlay.fontColor;
 import static org.osmdroid.views.overlay.gridlines.LatLonGridlineOverlay.fontSizeDp;
@@ -68,74 +60,11 @@ public class MapViewActivity extends Activity {
 
 
         map = findViewById(R.id.map);
-        map.setUseDataConnection(false);
+        //map.setUseDataConnection(false);
 
-        File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/osmdroid/");
-        if (f.exists()) {
+        String[] urls = {"http://127.0.0.1"};
 
-            File[] list = f.listFiles();
-            if (list != null) {
-                for (int i = 0; i < list.length; i++) {
-                    if (list[i].isDirectory()) {
-                        continue;
-                    }
-                    String name = list[i].getName().toLowerCase();
-                    if (!name.contains(".")) {
-                        continue; //skip files without an extension
-                    }
-                    name = name.substring(name.lastIndexOf(".") + 1);
-                    if (name.length() == 0) {
-                        continue;
-                    }
-                    if (ArchiveFileFactory.isFileExtensionRegistered(name)) {
-                        try {
-
-                            //ok found a file we support and have a driver for the format, for this demo, we'll just use the first one
-
-                            //create the offline tile provider, it will only do offline file archives
-                            //again using the first file
-                            OfflineTileProvider tileProvider = null;
-                            try {
-                                tileProvider = new OfflineTileProvider(new SimpleRegisterReceiver(ctx),
-                                        new File[]{list[i]});
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-
-                            //tell osmdroid to use that provider instead of the default rig which is (asserts, cache, files/archives, online
-                            map.setTileProvider(tileProvider);
-
-                            //this bit enables us to find out what tiles sources are available. note, that this action may take some time to run
-                            //and should be ran asynchronously. we've put it inline for simplicity
-
-                            String source = "";
-                            IArchiveFile[] archives = tileProvider.getArchives();
-                            if (archives.length > 0) {
-                                //cheating a bit here, get the first archive file and ask for the tile sources names it contains
-                                Set<String> tileSources = archives[0].getTileSources();
-                                //presumably, this would be a great place to tell your users which tiles sources are available
-                                if (!tileSources.isEmpty()) {
-                                    //ok good, we found at least one tile source, create a basic file based tile source using that name
-                                    //and set it. If we don't set it, osmdroid will attempt to use the default source, which is "MAPNIK",
-                                    //which probably won't match your offline tile source, unless it's MAPNIK
-                                    source = tileSources.iterator().next();
-                                    this.map.setTileSource(FileBasedTileSource.getSource(source));
-                                } else {
-                                    this.map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
-                                }
-
-                            } else {
-                                this.map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
-                            }
-
-                        } catch (Exception ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                }
-            }
-        }
-
+        map.setTileSource(new XYTileSource("tiles", 2, 17, 256, ".PNG", urls));
 
         IMapController mapController = map.getController();
         mapController.setZoom(15);
